4a855b6f77afae6b8de0c735fd8c730d13470f65
ahmet sekmen <ahmet.sekmen@hotmail.com>
2019-12-12 23:34:25 +0300

    PartialCalss

---
+++ BlazorClient/BlazorClient/Client/Pages/Depo.razor.cs
@@ -0,0 +1,23 @@
+﻿using BlazorClient.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorClient.Client.Pages
+{
+    public partial class Depo
+    {
+        List<Malzeme> malzemeList;
+        [Inject] BlazorClient.Client.Helper.InMemory inMemory { get; set; }
+
+
+        protected override void OnInitialized()
+        {
+            malzemeList = inMemory.GetMalzemeList();
+
+
+        }
+    }
+}
