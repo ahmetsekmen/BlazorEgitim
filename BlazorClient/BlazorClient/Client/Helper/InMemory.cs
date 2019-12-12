afc2fc6e1469c9c3a2807734d94304e1cbc3a204
ahmet sekmen <ahmet.sekmen@hotmail.com>
2019-12-12 23:15:57 +0300

    dependency Injection

---
+++ BlazorClient/BlazorClient/Client/Helper/InMemory.cs
@@ -0,0 +1,22 @@
+﻿using BlazorClient.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorClient.Client.Helper
+{
+    public class InMemory
+    {
+        public List<Malzeme> GetMalzemeList()
+        {
+            return new List<Malzeme>()
+            {   
+                new Malzeme(){Ismi="Masa",Uzunlugu=80},
+                new Malzeme(){Ismi="sandalye",Uzunlugu=100},
+                new Malzeme(){Ismi="Vazor",Uzunlugu=30}
+            };
+        }
+    }
+}
--- BlazorClient/BlazorClient/Client/Startup.cs
+++ BlazorClient/BlazorClient/Client/Startup.cs
@@ -1,3 +1,4 @@
+using BlazorClient.Client.Helper;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +8,7 @@ namespace BlazorClient.Client
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<InMemory>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
