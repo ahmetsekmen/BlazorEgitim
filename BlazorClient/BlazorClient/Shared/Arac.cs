620d2554ba5f62987b6bd2e74967b0481f14c879
Ahmet <ahmet.sekmen@hotmail.com>
2020-01-06 02:13:46 +0300

    ghjgj

---
+++ BlazorClient/BlazorClient/Shared/Arac.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorClient.Shared
+{
+    public class Arac
+    {
+        public Guid Id { get; set; }
+        public String Marka { get; set; }
+    }
+}
