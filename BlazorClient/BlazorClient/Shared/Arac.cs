6ed2185a7ca85487acb29206cefaa85d597dc5ee
Ahmet <ahmet.sekmen@hotmail.com>
2020-01-11 20:44:56 +0300

    validasyon

--- BlazorClient/BlazorClient/Shared/Arac.cs
+++ BlazorClient/BlazorClient/Shared/Arac.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace BlazorClient.Shared
     public class Arac
     {
         public Guid Id { get; set; }
+        [Required (ErrorMessage = "Marka bilgisi girmek zorundasınız.")]
         public String Marka { get; set; }
     }
 }
