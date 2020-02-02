afd88479b9584f651c7e690eecfd2c95e60bfce8
Ahmet <ahmet.sekmen@hotmail.com>
2020-02-02 17:13:10 +0300

    Auth auz gizleme

--- BlazorClient/BlazorClient/Client/Auth/OrnekAuthenticationStateProvider.cs
+++ BlazorClient/BlazorClient/Client/Auth/OrnekAuthenticationStateProvider.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,8 +12,13 @@ namespace BlazorClient.Client.Auth
     {
         public async override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            await Task.Delay(3000);
-            var anonim = new System.Security.Claims.ClaimsIdentity();
+            await Task.Delay(500);
+            var anonim = new System.Security.Claims.ClaimsIdentity(new List<Claim>() 
+            { 
+                new Claim("Key","Value"),
+                new Claim(ClaimTypes.Name,"Ahmet")
+                //new Claim(ClaimTypes.Role,"Manager")
+            });
             return await Task.FromResult(new AuthenticationState(new System.Security.Claims.ClaimsPrincipal(anonim)));
         }
     }
